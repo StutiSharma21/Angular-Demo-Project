2128390ad9a71c8219bbe173fdeed4f1cf9b78c0
Stuti Sharma <stuti.sharma@accolite.com>
2022-12-11 22:18:53 +0530

    services

---
+++ src/app/employee.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor() { }
+
+  getEmployees(){
+    return [
+      {"id" : 1,"name" : "Andrew","age" : 24},
+      {"id" : 2,"name" : "Nitin","age" : 25},
+      {"id" : 3,"name" : "Rohit","age" : 26},
+      {"id" : 4,"name" : "Mahesh","age" : 46},
+    ];
+
+  }
+}
